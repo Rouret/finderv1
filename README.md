370fc6352392135a16d51cd1bb9f96b8ec9029d9
Lucas Rouret <lucas.rouretperso@gmail.com>
2022-09-01 17:32:56 -0400

    a

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ### Finder v1
 
 We'll find u O_o
+
+## Aya
+
+```bash
+pip install -r requirements.txt
+```
--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 import sys
 import signal
+import argparse
+from src import printcolors as pc
+
+def _quit():
+	pc.printcolors("Bey World\n",pc.RED)
+	sys.exit(0)
+
+commands = {
+'quit': _quit
+}
+
+
+_quit()
+
+
+
+
