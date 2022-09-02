65d55a14274243eb24ac58d6fba15ae20ca9a7e0
Lucas ROURET <lucas.rouretperso@gmail.com>
2022-09-01 21:18:57 -0400

    add FinalRecon

--- src/Finder.py
+++ src/Finder.py
@@ -12,7 +12,8 @@ class Finder:
             "help": self.help,
             "quit": self._quit,
             "reload_dependencies": self.start,
-            "wb_username": self.nexfil,
+            "username": self.nexfil,
+            "url": self.finalrecon,
             "clear" : fp.fclear
         }
         self.vendor_folder = "vendor"
@@ -20,33 +21,27 @@ class Finder:
 
         self.python_exe = sys.executable
         
-    def __setPrompt(self,routes = ""):
-        self.prompt = self.name + "/" + "/".join(routes) + ">>> "
-
-
     def start(self):
         self.test()
         fp.fprint("Finder starter is starting ...", fp.YELLOW)
         #nexfil
-        fp.fprint("nexfil https://github.com/thewhiteh4t/nexfil", fp.MAGENTA)
-        self.__clone("nexfil","https://github.com/thewhiteh4t/nexfil.git", self.vendor_folder+"/nexfil")
-        fp.fprint("Installing dependencies of nexfill ...", fp.MAGENTA)
-        self.__pip_install(self.vendor_folder+"/nexfil/requirements.txt")
-        fp.fprint("nexfil DONE", fp.GREEN)
+        self.__install_by_pip("nexfil","https://github.com/thewhiteh4t/nexfil")
 
+        #FinalRecon
+        if os.name == "nt":
+            fp.fprint("FinalRecon is not supported on Windows", fp.RED)
+        else:
+            self.__install_by_pip("FinalRecon","https://github.com/thewhiteh4t/FinalRecon")
         #END
         fp.fprint("\nFinder is ready to find u O_o\n", fp.YELLOW)
-        
-        
-    def __clone(self,lib_name, url, folder):
-        if(os.path.exists(folder)):
-            fp.fprint(lib_name+" is already cloned", fp.GREEN)
-        else:
-            subprocess.run(["git", "clone", url, folder])
 
-    def __pip_install(self, requirement_file):
-       subprocess.run(["pip", "install", "-r", requirement_file], stdout=subprocess.DEVNULL,stderr=subprocess.STDOUT)
-            
+    def __install_by_pip(self, lib_name, github_url):
+        fp.fprint(lib_name + " " + github_url, fp.MAGENTA)
+        self.__clone(lib_name, github_url+".git", self.vendor_folder+"/"+lib_name)
+        fp.fprint("Installing dependencies of "+lib_name+" ...", fp.MAGENTA)
+        self.__pip_install(self.vendor_folder+"/"+lib_name+"/requirements.txt")
+        fp.fprint(lib_name+" DONE", fp.GREEN)
+
     def nexfil(self):
         fp.fprint("The provided usernames are checked on over 350 websites within few seconds. (nexfil)", fp.YELLOW)
         options = ["username"]
@@ -64,6 +59,42 @@ class Finder:
         fp.fprint("Github: https://github.com/thewhiteh4t/nexfil\n", fp.GREEN)
         self.__setPrompt()
         
+    def finalrecon(self):
+        if os.name == "nt":
+            fp.fprint("FinalRecon is not supported on Windows", fp.RED)
+            return
+        fp.fprint("Goal of FinalRecon is to provide an overview of the target in a short amount of time while maintaining the accuracy of results. (FinalRecon)", fp.YELLOW)
+        options = ["url"]
+        routes = ["FinalRecon"]
+        self.__setPrompt(routes)
+        self.__display_options(routes,options)
+        result = self.__get_multiple_options(routes,options)
+        if not result: return
+        url = result["url"]
+        routes.append(url)
+        self.__setPrompt(routes)
+        finalrecon_folder = self.vendor_folder+"/FinalRecon"
+        subprocess.run([self.python_exe, "finalrecon.py" , "--full", url], cwd=os.path.abspath(finalrecon_folder))
+        fp.fprint("wb_username DONE for "+url + ", thanks to thewhiteh4t/FinalRecon", fp.GREEN)
+        fp.fprint("Github: https://github.com/thewhiteh4t/FinalRecon\n", fp.GREEN)
+        self.__setPrompt()
+        
+    def help(self):
+        for key in self.commands:
+            fp.fprint( "- " + key, fp.GREEN)
+
+    def __setPrompt(self,routes = ""):
+        self.prompt = self.name + "/" + "/".join(routes) + ">>> "
+
+    def __clone(self,lib_name, url, folder):
+        if(os.path.exists(folder)):
+            fp.fprint(lib_name+" is already cloned", fp.GREEN)
+        else:
+            subprocess.run(["git", "clone", url, folder],stdout=subprocess.DEVNULL,stderr=subprocess.STDOUT)
+
+    def __pip_install(self, requirement_file):
+       subprocess.run(["pip", "install", "-r", requirement_file], stdout=subprocess.DEVNULL,stderr=subprocess.STDOUT)
+
     def __display_options(self,routes,options):
         if len(routes) > 0:
             fp.fprint("\nOptions for " + routes[-1] + " :", fp.YELLOW)
@@ -89,17 +120,10 @@ class Finder:
             result[option] = temp_input
         return result
     
-
     def test(self):
         print(self.python_exe)
         return
         
-
-    def help(self):
-        for key in self.commands:
-            fp.fprint( "- " + key, fp.GREEN)
-    
- 
     def _quit(self):
         fp.fprint("We will find u anyway à_à", fp.RED)
         sys.exit(0)
